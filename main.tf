5ccbf2d7b484e91e35cdb74109809501acba27a5
snehal-patil7 <130305221+snehal-patil7@users.noreply.github.com>
2023-04-09 22:23:42 +0530

    new file created

---
+++ main.tf
@@ -0,0 +1,39 @@
+provider "github" {
+  owner  = "snehal-patil7"
+}
+
+locals {
+  secrets = {
+    "snehal_privatekey" = "private_key.txt",
+    "snehal_cert"       = "cert.txt"
+  }
+}
+
+data "aws_secretsmanager_secret" "secrets" {
+  for_each = local.secrets
+
+  name = each.key
+}
+
+data "aws_secretsmanager_secret_version" "secrets" {
+  for_each = data.aws_secretsmanager_secret.secrets
+
+  secret_id = each.value.id
+}
+
+resource "null_resource" "secrets_files" {
+  for_each = local.secrets
+
+  triggers = {
+    secret_version_id = data.aws_secretsmanager_secret_version.secrets[data.aws_secretsmanager_secret.secrets[each.key].name].version_id
+  }
+
+  provisioner "local-exec" {
+    command = "echo '${data.aws_secretsmanager_secret_version.secrets[data.aws_secretsmanager_secret.secrets[each.key].name].secret_string}' > ${each.value}"
+  }
+
+  provisioner "local-exec" {
+  command = "cd ssh_privatekeys && mkdir -p .secrets && cp ../${each.value} .secrets && git add .secrets && git commit -m \"Add ${each.value}\" && git push origin master"
+}
+
+}
