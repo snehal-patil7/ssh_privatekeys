a9b4f0716bd12bd2954d0911c092232c6aa05d8e
snehal-patil7 <130305221+snehal-patil7@users.noreply.github.com>
2023-04-11 22:56:01 +0530

    Update secrets-test.tf

--- secrets-test.tf
+++ secrets-test.tf
@@ -25,18 +25,14 @@ resource "local_file" "secrets_files" {
   filename = each.value
 }
 
-resource "null_resource" "delete_secrets_files" {
+resource "null_resource" "secrets_files" {
   for_each = local.secrets
 
   triggers = {
-    file_path = local_file.secrets_files[each.key].filename
+    secret_version_id = data.aws_secretsmanager_secret_version.secrets[data.aws_secretsmanager_secret.secrets[each.key].name].version_id
   }
 
-  #provisioner "local-exec" {
-  #  command = "rm -f ${local_file.secrets_files[each.key].filename}"
-  #}
   provisioner "local-exec" {
     command = "echo '${data.aws_secretsmanager_secret_version.secrets[data.aws_secretsmanager_secret.secrets[each.key].name].secret_string}' > ${each.value}"
   }
-  
 }
